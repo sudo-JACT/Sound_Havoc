d0501b0a5775b0afedc97dc49036a69e9c2711f4
1MD0NK3Y <jacopino.toma@gmail.com>
2024-04-24 19:34:09 +0200

    never mind

--- Sound_Havoc_Amplifier/src/Sound_Havoc_Amplifier.cpp
+++ Sound_Havoc_Amplifier/src/Sound_Havoc_Amplifier.cpp
@@ -1,27 +1,122 @@
 /*libs*/
 #include <stdlib.h>
+#include <iostream>
+#include <new>
+#include <stdexcept>
+#include <iostream>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
 #include <stddef.h>
 #include <stdint.h>
 #include <lv2.h>
 
+#define u8 uint8_t
+#define u16 uint16_t
 #define u32 uint32_t
+#define u64 uint64_t
+
+using namespace std;
 
 /*struct definition*/
 
-typedef struct {
+/*typedef struct {
 
     float* audio_in_ptr;
     float* audio_out_ptr;
     float* amp_ptr;
 
-} Sound_Havoc_Amplifier;
+} Sound_Havoc_Amplifier;*/
+
+class Amplifier {
+
+    private:
+
+        float* audio_in_ptr;
+        float* audio_out_ptr;
+        float* amp_ptr;
+
+    public:
+
+        Amplifier() {
+
+            audio_in_ptr = static_cast<float*> (nullptr);
+            audio_out_ptr = static_cast<float*> (nullptr);
+            amp_ptr = static_cast<float*> (nullptr);
+
+        }
+
+        float* getAudioIn() {
+
+            return audio_in_ptr;
+
+        }
+
+        float* getAudioOut() {
+
+            return audio_out_ptr;
+
+        }
+
+        float* getAmp() {
+
+            return amp_ptr;
+
+        }
+
+        void setAudioIn(float* source) {
+
+            audio_in_ptr = static_cast<float*> (source);
+
+        }
+
+        void setAudioOut(float* source) {
+
+            audio_out_ptr = static_cast<float*> (source);
+
+        }
+
+        void setAmp(float* source) {
+
+            amp_ptr = static_cast<float*> (source);
+
+        }
+
+        void run(u32 sample_count) {
+
+            if((!audio_in_ptr) || (!audio_out_ptr) || (!amp_ptr)) {
+
+                return;
+    
+            }
+
+            for(u32 i=0; i < sample_count; i++) {
+
+                audio_out_ptr[i] = audio_in_ptr[i] * *(amp_ptr);
+    
+            }
+
+        }
+
+};
 
 
 /*core methods*/
 //const char *URI
 static LV2_Handle instantiate(const struct LV2_Descriptor *descriptor, double sample_rate, const char *bundle_path, const LV2_Feature *const *features) {
 
-    Sound_Havoc_Amplifier* amp = (Sound_Havoc_Amplifier*) calloc(1, sizeof(Sound_Havoc_Amplifier));
+    Amplifier* amp = static_cast<Amplifier*> (nullptr);
+
+    try {
+        
+        Amplifier* amp = new Amplifier();
+    
+    }catch(const bad_alloc& b) {
+
+        cerr << "Memory error" << endl;
+        return nullptr;
+    
+    }
 
     return amp;
 
@@ -29,7 +124,7 @@ static LV2_Handle instantiate(const struct LV2_Descriptor *descriptor, double sa
 
 static void connect_port(LV2_Handle instance, uint32_t port, void *data_location) {
 
-    Sound_Havoc_Amplifier* amp = (Sound_Havoc_Amplifier*) instance;
+    Amplifier* amp = static_cast<Amplifier*> (instance);
 
     if(!amp) {
 
@@ -37,18 +132,20 @@ static void connect_port(LV2_Handle instance, uint32_t port, void *data_location
 
     }
 
+    float* tmp = static_cast<float*> (data_location);
+
     switch (port) {
 
         case 0:
-            amp->audio_in_ptr = (float*) data_location;
+            amp->setAudioIn(tmp);
             break;
 
         case 1:
-            amp->audio_out_ptr = (float*) data_location;
+            amp->setAudioOut(tmp);
             break;
 
         case 2:
-            amp->amp_ptr = (float*) data_location;
+            amp->setAmp(tmp);
             break;
 
         default:
@@ -65,19 +162,9 @@ static void activate(LV2_Handle instance) {
 
 static void run(LV2_Handle instance, uint32_t sample_count) {
 
-    Sound_Havoc_Amplifier* amp = (Sound_Havoc_Amplifier*) instance;
+    Amplifier* amp = static_cast<Amplifier*> (instance);
 
-    if((!amp) || (!amp->audio_in_ptr) || (!amp->audio_out_ptr) || (!amp->amp_ptr)) {
-
-        return;
-    
-    }
-
-    for(u32 i=0; i < sample_count; i++) {
-
-        amp->audio_out_ptr[i] = amp->audio_in_ptr[i] * *(amp->amp_ptr);
-    
-    }
+    amp->run(sample_count);
     
     
 
@@ -91,7 +178,7 @@ static void deactivate(LV2_Handle instance) {
 
 static void cleanup(LV2_Handle instance) {
 
-    Sound_Havoc_Amplifier* amp = (Sound_Havoc_Amplifier*) instance;
+    Amplifier* amp = static_cast<Amplifier*> (instance);
 
     if(!amp) {
 
@@ -99,7 +186,7 @@ static void cleanup(LV2_Handle instance) {
 
     }
 
-    free(amp);
+    delete amp;
 
 }
 
